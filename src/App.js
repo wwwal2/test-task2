428dc08f42cd452a114c71de0d1baea9855b2413
wwwal2 <smalex9@gmail.com>
2022-01-29 12:25:23 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Cadeau Concepten Coding Assesment</h1>
+      <h2>
+        We expect you to create your own fork with the application tha
+        implements the requirements described by the link:
+      </h2>
+      <a
+        href="https://docs.google.com/document/d/1vR53E6GDTWqreqEGNybGeKq1p8kA7uCYUvbdgHCKtiM/edit?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://docs.google.com/document/d/1vR53E6GDTWqreqEGNybGeKq1p8kA7uCYUvbdgHCKtiM/edit?usp=sharing
+      </a>
+      <h3>
+        There is no any time restrictions, however we expect this task to take
+        ~1hr
+      </h3>
+    </div>
+  );
+}
